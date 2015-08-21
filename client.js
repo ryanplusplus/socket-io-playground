51e5a7b13a9def2c3ffbb2f30953c9c875450730
Ryan Hartlage <ryanplusplus@gmail.com>
2015-08-20 20:38:37 -0400

    Seems to be working

--- client.js
+++ client.js
@@ -1,2 +1,13 @@
 var socket = io();
-console.log('maybe created a socket?');
+
+$(document).ready(function() {
+  $('#send-form').submit(function(e) {
+    socket.emit('message', $('#message-to-send').val());
+    $('#message-to-send').val('');
+    e.preventDefault();
+  });
+
+  socket.on('message', function(message) {
+    $('#received-messages').append($('<li>').text(message));
+  });
+});
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var socketio = require('socket.io')(http);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static('.'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-socketio.on('connection', function(socket) {
-  console.log('connection');
+io.on('connection', function(socket) {
+  console.log('connected');
+
+  socket.on('message', function(message) {
+    console.log('message: ' + message);
+    io.emit('message', 'someone sent me a message: ' + message);
+  });
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
